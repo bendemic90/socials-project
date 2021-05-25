22c9d25403ed03d1215f0b6eb716dcc088bb2cbe
Ben T <ben.trevethan@gmail.com>
2021-05-25 19:14:54 +0930

    env variables and responsiveness

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-//require("dotenv").config();
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js'
 
 const app = express()
+dotenv.config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: true }));
@@ -13,13 +14,10 @@ app.use(cors())
 
 app.use('/posts', postRoutes);
 
-const mongoConnect = "";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(mongoConnect, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message))
 
 mongoose.set('useFindAndModify', false);
-
-
